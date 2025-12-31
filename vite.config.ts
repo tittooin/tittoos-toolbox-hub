231405617cbd5e13a0560f65d8b48d9a2438bb92
Hugging Face CLI <user@example.com>
2026-01-01 02:19:18 +0530

    fix(config): update COEP to credentialless to allow 3rd party scripts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Embedder-Policy": "credentialless",
     },
   },
   plugins: [
